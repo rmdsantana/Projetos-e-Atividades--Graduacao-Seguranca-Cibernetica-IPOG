5116d27d758bfbac960ca60618a04ae1587f1cb0
Raimundo Santana <146298095+rmdsantana@users.noreply.github.com>
2025-06-11 09:28:28 -0300

    Create instalacao-docker.sh

---
+++ 01Sem--Banco_de_Dados-I/instalacao-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Instalação do Docker seguindo a documentação oficial
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#Pós instalação
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Subir o docker
+docker compose up -d
