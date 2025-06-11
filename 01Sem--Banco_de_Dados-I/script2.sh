0c6f50f8531985db0ff02329aca190fb193340d9
Raimundo Santana <146298095+rmdsantana@users.noreply.github.com>
2025-06-11 12:24:52 -0300

    Update script2.sh

--- 01Sem--Banco_de_Dados-I/script2.sh
+++ 01Sem--Banco_de_Dados-I/script2.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 #Pós instalação
-echo "Adicionando o usuário ao grupo 'docker'..."
-sudo usermod -aG docker $USER
-
-# Garantir que o grupo Docker esteja ativo
-newgrp docker
-
-# Subir o docker
+#Subir o docker
+sleep [5]
 docker compose up -d
